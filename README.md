9c124e57ac201b3743af50478fd76f74f9f01654
nubby0208 <nubby0208@outlook.com>
2023-09-17 07:42:45 +0900

    db-readme

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # funtoken-test
-1. move into the backend folder and open command prompt and enter npm install, after which enter node index.js
+
+1. import products.sql into mysql database and start MySQL server
+2. inside the backend folder open command prompt and enter npm install, after which enter node index.js
 	backend api server is running
-2. move into the react-test folder and open command prompt and enter npm install, after which enter npm start
+3. inside the react-test folder open command prompt and enter npm install, after which enter npm start
 	react frontend page will open
 	open another command prompt and enter npm test which will generate test result
-3. move into the yii-test folder and open command prompt to enter composer install, after which enter php yii serve
+4. inside into the yii-test folder open command prompt to enter composer install, after which enter php yii serve
 	yii application page will open
 	open another command prompt and enter vendor/bin/codecept run unit which will generate test result
---
+++ products.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 17, 2023 at 12:39 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(11) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`) VALUES
+(1, 'aaa', '', 10),
+(2, 'hkh', '', 60),
+(3, 'j', '', 60),
+(4, 'ds', '', 70),
+(5, 'fhh', '', 80),
+(6, 'ytyi', '', 40),
+(7, 'asgd', '', 40),
+(8, 'df', '', 30);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
