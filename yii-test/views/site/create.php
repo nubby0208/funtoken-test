b4f8d8f59a1b8fdb2370b75bc72d0dc1c74d8ea5
nubby0208 <nubby0208@outlook.com>
2023-09-16 18:21:50 +0900

    yii-create view

---
+++ yii-test/views/site/create.php
@@ -0,0 +1,13 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$this->title = 'Create Product';
+$this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<?= $this->render('_form', ['product' => $product]) ?>
