ebe5d24bd6ec89490f5a1a5dc6e97931a2dd6a2e
nubby0208 <nubby0208@outlook.com>
2023-09-16 17:53:38 +0900

    yii-dbconfig

--- yii-test/config/db.php
+++ yii-test/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic_test',
+    'dsn' => 'mysql:host=localhost;dbname=mysql',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
